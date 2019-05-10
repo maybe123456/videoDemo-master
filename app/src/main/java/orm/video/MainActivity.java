867f504c699ac221f1cf8421e46d3603e6b73ecc
maybe123456 <762922419@qq.com>
2019-05-10 12:37:19 +0800

    首次提交

---
+++ app/src/main/java/orm/video/MainActivity.java
@@ -0,0 +1,29 @@
+package orm.video;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+import io.vov.vitamio.Vitamio;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+        getApplicationContext().getExternalFilesDir("");
+        Vitamio.initialize(MainActivity.this);
+        Intent intent=new Intent(MainActivity.this,VideoViewActivity2.class);
+//        Intent intent=new Intent(MainActivity.this,VitamioActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+}
